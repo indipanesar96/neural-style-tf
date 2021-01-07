3b7e6e9ebe9b00548bef11504d806287b56cba48
indipanesar@gmail.com <Jammin96Creative13>
2021-01-07 16:57:14 +0000

    2to3'd neural_style.py to python3-ify the main scripts. Removed checks for the stylize_image.sh for a faster dev

--- stylize_image.sh
+++ stylize_image.sh
@@ -8,23 +8,6 @@ if [ "$#" -le 1 ]; then
    exit 1
 fi
 
-echo ""
-read -p "Did you install the required dependencies? [y/n] $cr > " dependencies
-
-if [ "$dependencies" != "y" ]; then
-  echo "Error: Requires dependencies: tensorflow, opencv2 (python), scipy"
-  exit 1;
-fi
-
-echo ""
-read -p "Do you have a CUDA enabled GPU? [y/n] $cr > " cuda
-
-if [ "$cuda" != "y" ]; then
-  device='/cpu:0'
-else
-  device='/gpu:0'
-fi
-
 # Parse arguments
 content_image="$1"
 content_dir=$(dirname "$content_image")
@@ -35,10 +18,10 @@ style_dir=$(dirname "$style_image" )
 style_filename=$(basename "$style_image")
 
 echo "Rendering stylized image. This may take a while..."
-python neural_style.py \
+python3 neural_style.py \
 --content_img "${content_filename}" \
 --content_img_dir "${content_dir}" \
 --style_imgs "${style_filename}" \
 --style_imgs_dir "${style_dir}" \
---device "${device}" \
+--device '/cpu:0' \
 --verbose;
